9e929bb6e3bb0cd998e319781b9b481a62e9f976
RIYA MISHRA <itsriya1586@gmail.com>
2026-07-09 10:36:44 +0530

    Interview Tip (1-Minute Revision)

---
+++ Day_10_Interview Tip (1-Minute Revision).sql
@@ -0,0 +1,13 @@
+ --Interview Tip (1-Minute Revision)
+
+-- ✅ WHERE → Rows filter karta hai.
+-- ✅ HAVING → Groups filter karta hai.
+-- ✅ DELETE → Rows delete.
+-- ✅ TRUNCATE → Data delete, structure rehti hai.
+-- ✅ DROP → Data + structure dono delete.
+-- ✅ PRIMARY KEY → Unique identifier.
+-- ✅ FOREIGN KEY → Tables ke beech relationship.
+-- ✅ GROUP BY → Group banakar aggregate functions use karne ke liye.
+-- ✅ COUNT(*) → Sabhi rows.
+-- ✅ COUNT(column) → Sirf non-NULL values.
+              
